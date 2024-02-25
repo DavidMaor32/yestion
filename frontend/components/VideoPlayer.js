3d6c67a6769e74a901979a6121dddc7d11a29706
DavidMaor32 <126754136+DavidMaor32@users.noreply.github.com>
2024-02-25 19:47:20 +0200

    changed controls

--- frontend/components/VideoPlayer.js
+++ frontend/components/VideoPlayer.js
@@ -1,6 +1,6 @@
 export default function VideoPlayer({ src }) {
     return (
-        <video controls autoPlay muted>
+        <video controls>
             <source src={src} type="video/mp4" />
             Your browser does not support the video tag.
         </video>
